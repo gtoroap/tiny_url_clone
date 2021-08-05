da30ec2401b0581a395ed2954e8394305639c927
Gustavo Toro <gtoroap@gmail.com>
2021-08-05 08:03:11 -0500

    Validates errors when creating urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
 class Url < ApplicationRecord
+  URL_REGEX = %r(\A(?:https?://)?(?:\w+\.)(?:\.?\w{2,})+\z).freeze
 
+  validates :original, presence: true, format: { with: URL_REGEX, message: 'please enter a correct url'}
   validates :slug, uniqueness: true, allow_blank: false
 
   def generate_short_url
