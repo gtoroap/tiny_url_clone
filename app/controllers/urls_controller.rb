915ebb3a68c1650928a29a9aa5968eef1b07edca
Gustavo Toro <gtoroap@gmail.com>
2021-08-05 07:59:28 -0500

    Url create action modified

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -7,8 +7,23 @@ class UrlsController < ApplicationController
   end
 
   def create
+    @url = Url.new(url_params)
+    @url.generate_short_url
+
+    if @url.save
+      redirect_to urls_path
+    else
+      flash.now[:error] = 'Please, provide a correct Url.'
+      render 'new'
+    end
   end
 
   def short
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:original)
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ApplicationRecord
+
+  validates :slug, uniqueness: true, allow_blank: false
+
+  def generate_short_url
+    self.slug = SecureRandom.urlsafe_base64(5).downcase
+  end
 end
